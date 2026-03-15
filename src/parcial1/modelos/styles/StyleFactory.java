5191a212236a16c0a1e151811a78f758cfb316c5
juanx199 <jccarvajalcamargo@gmail.com>
2026-03-15 12:28:10 -0500

    Añadí los exporters, parte del Usuario y Stylefactory, e hice una prueba de funcionalidad en el main, si se reutiliza un mismo tipo de letra, color y tamaño dice que lo está reutilizando

--- src/parcial1/modelos/styles/StyleFactory.java
+++ src/parcial1/modelos/styles/StyleFactory.java
@@ -4,10 +4,23 @@
  */
 package parcial1.modelos.styles;
 
+import java.util.HashMap;
+import java.util.Map;
 /**
  *
  * @author JUANCA
  */
 public class StyleFactory {
+    private static final Map<String, TextStyle> styles = new HashMap<>();
     
+    public static TextStyle getStyle(String font, int size, String color){
+        String key = font + size + color; //Clave univa
+        if(!styles.containsKey(key)){
+            styles.put(key, new TextStyle(font, size, color));
+            
+        }else{
+            System.out.println(" Reutilizando el estilo:" + key);
+        }
+        return styles.get(key);
+    }
 }
